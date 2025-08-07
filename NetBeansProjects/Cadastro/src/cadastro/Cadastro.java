b77bdbb79927fc7afd6438cb3ab476a4c12a45a0
ViniPetry <viniciusgarciapetry@gmail.com>
2025-08-07 10:49:29 -0300

    Add files via upload

---
+++ NetBeansProjects/Cadastro/src/cadastro/Cadastro.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author VINICIUSGARCIAPETRY
+ */
+import java.util.Scanner;
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner ler= new Scanner(System.in);
+        String nome= ler.next();
+        System.out.println("Bom dia "+nome);
+        
+    }
+    
+}
---
+++ NetBeansProjects/OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olamundo;
+
+/**
+ *
+ * @author VINICIUSGARCIAPETRY
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome= "Vinicius";
+        String sobrenome= "Marques";
+        System.out.println("Ola Mundo!");
+        System.out.println("Bomd dia "+nome);
+        System.out.println(nome+" "+sobrenome);
+        
+        
+        
+        
+    }
+    
+}
